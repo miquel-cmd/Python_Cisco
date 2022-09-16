29873a2e9def93812ce4d28034cb6b7bf1c88b0a
miquel-cmd <65029269+miquel-cmd@users.noreply.github.com>
2022-09-16 15:44:39 +0200

    new commit

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 import csv
 import time
 from icmplib import *
+import sys
+import traceback
 
 def csv_to_json(csvFilePath):
 	jsonArray = []
@@ -20,7 +22,7 @@ def csv_to_json(csvFilePath):
 	return(jsonArray)
 
 
-csvFilePath='data.csv'
+csvFilePath='input.csv'
 array_json=csv_to_json(csvFilePath)
 
 start_process=time.time()
@@ -36,30 +38,29 @@ for i in array_json:
 		'verbose':True
 	}
 	print(dev1)
-	status=ping(i["ip"],count = 2,interval=0.2)
+	status=ping(i["ip"],count = 2)
 	try:
 		start_equipment=time.time()
 		router_connection = ConnectHandler(**dev1)
 		router_connection.enable()
 		hostname = router_connection.send_command('show run | i hostname',expect_string='#')
-		print('hostname: ',hostname)
+		#print('hostname: ',hostname)
 		prompt=hostname.split(' ')[1].split('\n')[0].strip(" ")+'#'
-		print('prompt:', prompt, 'hay salto')
-		#router_connection.send_command('term datadump', expect_string='#')
+		#print('prompt:', prompt, 'hay salto')
+		router_connection.send_command('term datadump', expect_string='#')
 		#showrun=router_connection.send_command('show running', expect_string=prompt)
 		#showinv=router_connection.send_command('show inventory',expect_string=prompt)
 		#showver=router_connection.send_command('show version',expect_string=prompt)
 		router_connection.disconnect()
-		file=prompt
+		#file=prompt
 		#f= open(prompt+i["ip"]+'.txt',"w")
 		#f.write("show running\n" + showrun +"\n\nshow inventory\n" + showinv + "\n\nshow version \n"+showver)
 		#f.close()
-		f.write( i["ip"] + "," + prompt +","+str(ping.is_alive)+"\n")
-		print(f"Router {hostname} tarda:{time.time()-start_equipment}\n")
+		f.write( i["ip"] + "," + "OK" +","+str(status.is_alive)+"\n")
+		#print(f"Router {hostname} tarda:{time.time()-start_equipment}\n")
 	except Exception as e:
-		#print('error', str(e))
-		f.write( i["ip"] + ","+str(e) +",",str(ping.is_alive)+'\n')
-  		#router_connection.disconnect()
+		#print('errorrrrrrrrrrrrrrrrrrrrr' , str(e).split('\n')[0])
+		f.write( i["ip"] + ","+str(e).split('\n')[0] +","+str(status.is_alive)+'\n')
 		continue
 print(f"El proceso ha tardardo un tiempo de: {time.time()-start_process}")
 f.close()
